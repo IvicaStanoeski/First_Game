bde8d38a18bc5a745bc7e40e43897d1e6a922392
IvicaStanoeski <ivica.stanoeski@hotmail.com>
2018-10-18 15:35:21 +0200

    Camera and background
    The background is set and a camera over it that moves for a certain period of time

---
+++ Arkaden Shooter so Avijoncinja/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    float speed = 0.2f;
+    float counter = 0f;
+    float limit = 1000000f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (counter < limit)
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+            counter++;
+        }
+    }
+}
